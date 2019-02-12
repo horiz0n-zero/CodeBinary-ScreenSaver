6d42b4211249452667f5c0714efff37c8c12f74c
horiz0n-zero <antoinefeufeu>
2019-02-12 17:27:06 +0100

    Initial Commit

---
+++ CodeBinary/CodeBinaryView.h
@@ -0,0 +1,13 @@
+//
+//  CodeBinaryView.h
+//  CodeBinary
+//
+//  Created by Antoine Feuerstein on 12/02/2019.
+//  Copyright © 2019 Antoine Feuerstein. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface CodeBinaryView : ScreenSaverView
+
+@end
---
+++ CodeBinary/CodeBinaryView.m
@@ -0,0 +1,52 @@
+//
+//  CodeBinaryView.m
+//  CodeBinary
+//
+//  Created by Antoine Feuerstein on 12/02/2019.
+//  Copyright © 2019 Antoine Feuerstein. All rights reserved.
+//
+
+#import "CodeBinaryView.h"
+
+@implementation CodeBinaryView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
